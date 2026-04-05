621f8b35acb9f95515707345d7aca88230c8f101
luizhakan <luizhakan2@protonmail.com>
2026-04-04 23:35:31 -0300

    refatora a declaração da variável versao e remove a variável idade; ajusta a exibição de comandos

--- hello.go
+++ hello.go
@@ -2,21 +2,25 @@ package main // É o principal pacote do programa
 
 import (
 	"fmt"
-	"reflect"
 ) // pacote de formatação
 
 func main() {
 	// Não recebe e nem retorna nada
 	nome := "Luiz" // o := sem a palavra reservada var, quer dizer que eu estou a criar a variável e a inferir o tipo nela automaticamente
-	var versao float32 = 1.2
-	var idade int = 23
+	versao := 1.2
+	var comando int
 
 	// nome = 12 // vai dar erro, por que o tipo inferido para nome foi string
 	fmt.Println("Olá sr.", nome)
-	fmt.Println("Sua idade é:", idade)
 	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("O tipo de nome é:", reflect.TypeOf(nome))
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("3 - Sair do programa")
+
+	fmt.Scan(&comando)
+
+	fmt.Println("Comando", comando)
 }
 
 // fazer o build com go build arquivo.go
