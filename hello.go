c1f26af4b1300a054781e006ab98dea14fb0b485
luizhakan <luizhakan2@protonmail.com>
2026-04-04 23:02:00 -0300

    first commit

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main // É o principal pacote do programa
+
+import "fmt" // pacote de formatação
+
+func main() {
+	// Não recebe e nem retorna nada
+	fmt.Println("Hello world!")
+}
+
+// fazer o build com go build arquivo.go
+// compila e já executa: go run arquivo.go
