18be26349fc887c7382cdc2e71ecaee12311ac23
luizhakan <luizhakan2@protonmail.com>
2026-04-04 23:13:01 -0300

    substitui mensagem de saudação por informações do usuário

--- hello.go
+++ hello.go
@@ -1,10 +1,20 @@
 package main // É o principal pacote do programa
 
 import "fmt" // pacote de formatação
+import "reflect"
 
 func main() {
 	// Não recebe e nem retorna nada
-	fmt.Println("Hello world!")
+	var nome = "Luiz"
+	var versao float32 = 1.2
+	var idade int = 23
+
+	// nome = 12 // vai dar erro, por que o tipo inferido para nome foi string
+	fmt.Println("Olá sr.", nome)
+	fmt.Println("Sua idade é:", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo de nome é:", reflect.TypeOf(nome))
 }
 
 // fazer o build com go build arquivo.go
