54dcf1854d1ab7f8b586ae107c874578e7f14d4f
luizhakan <luizhakan2@protonmail.com>
2026-04-05 22:58:50 -0300

    refatora a função main para utilizar funções auxiliares e melhora a legibilidade do código

--- hello.go
+++ hello.go
@@ -2,24 +2,13 @@ package main // É o principal pacote do programa
 
 import (
 	"fmt"
+	"os"
 ) // pacote de formatação
 
 func main() {
 	// Não recebe e nem retorna nada
-	nome := "Luiz" // o := sem a palavra reservada var, quer dizer que eu estou a criar a variável e a inferir o tipo nela automaticamente
-	versao := 1.2
-	var comando int
-
-	// nome = 12 // vai dar erro, por que o tipo inferido para nome foi string
-	fmt.Println("Olá sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("3 - Sair do programa")
-
-	fmt.Scan(&comando)
-
+	exibeIntroducao()
+	exibeMenu()
 	// fmt.Println("Comando", comando)
 
 	// if comando == 1 {
@@ -32,7 +21,7 @@ func main() {
 	// 	fmt.Println("Comando inválido")
 	// }
 
-	switch comando {
+	switch leComando() {
 	case 1:
 		fmt.Println("Iniciar monitoramento selecionado")
 
@@ -41,11 +30,35 @@ func main() {
 
 	case 3:
 		fmt.Println("Saindo...")
+		os.Exit(0)
 
 	default:
 		fmt.Println("Comando inválido")
+		os.Exit(-1)
 	}
 }
 
 // fazer o build com go build arquivo.go
 // compila e já executa: go run arquivo.go
+
+func exibeIntroducao() {
+	nome := "Luiz" // o := sem a palavra reservada var, quer dizer que eu estou a criar a variável e a inferir o tipo nela automaticamente
+	versao := 1.2
+
+	// nome = 12 // vai dar erro, por que o tipo inferido para nome foi string
+	fmt.Println("Olá sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("3 - Sair do programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+
+	return comandoLido
+}
