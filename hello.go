9f93ea92ab59ee44ebc960efb21ca7058c4bd759
luizhakan <luizhakan2@protonmail.com>
2026-04-11 23:43:07 -0300

    adiciona timestamp ao registro de status dos sites no log

--- hello.go
+++ hello.go
@@ -210,7 +210,7 @@ func registraLog(site string, status bool) {
 		fmt.Println("Ocorreu um erro:", erro)
 	}
 
-	arquivo.WriteString(site + "- online:" + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "- online:" + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
 }
